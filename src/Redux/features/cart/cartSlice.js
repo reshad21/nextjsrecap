5353106661e90a70e31ca370fbe0e65968aadb6f
reshad21 <miakala7@gmail.com>
2023-10-22 23:00:09 +0600

    fomating cartslice

--- src/Redux/features/cart/cartSlice.js
+++ src/Redux/features/cart/cartSlice.js
@@ -3,6 +3,7 @@ const { createSlice } = require("@reduxjs/toolkit")
 
 const initialState = {
     cart: [],
+    // price: null,
 }
 
 export const cartSlice = createSlice({
@@ -10,23 +11,21 @@ export const cartSlice = createSlice({
     initialState,
     reducers: {
         addToCart: (state, action) => {
-            const productToAdd = action.payload;
-            const existingProduct = state.cart.find((product) => product.id === productToAdd.id);
-
+            const existingProduct = state.cart.find((product) => product.id === action.payload.id);
             if (existingProduct) {
                 existingProduct.quantity += 1;
             } else {
                 state.cart.push({ ...productToAdd, quantity: 1 });
             }
         },
-        increase: (state, action) => {
-            const productToIncrease = action.payload;
-            const existingProduct = state.cart.find((product) => product.id === productToIncrease.id);
 
+        increase: (state, action) => {
+            const existingProduct = state.cart.find((product) => product.id === action.payload.id);
             if (existingProduct) {
                 existingProduct.quantity += 1;
             }
         },
+
         decrease: (state, action) => {
             state.cart = state.cart.filter((product) => {
                 if (product.id === action.payload.id) {
