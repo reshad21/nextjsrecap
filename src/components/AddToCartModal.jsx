06da2ef25e6b05bd802b740956a40a7d4db0cb13
reshad21 <miakala7@gmail.com>
2023-10-28 11:22:54 +0600

    check out btn add

--- src/components/AddToCartModal.jsx
+++ src/components/AddToCartModal.jsx
@@ -3,7 +3,7 @@ import Cart from "./Cartpage/Cart";
 const AddToCartModal = ({ closeModal }) => {
     return (
         <div className="absolute top-[90px] right-[40px] shadow-2xl bg-blue-50">
-            <div className="relative border border-green-600 rounded-md px-3 pt-10 w-96 h-[80vh] overflow-y-auto">
+            <div className="relative border border-green-600 rounded-md px-3 pt-10 pb-3 w-96 max-h-[80vh] overflow-y-auto">
                 <button onClick={() => closeModal(false)} className="text-xl font-bold absolute top-0 right-0"><TiDelete size={30} /></button>
                 <Cart />
             </div>
--- src/components/Cartpage/Cart.jsx
+++ src/components/Cartpage/Cart.jsx
@@ -1,5 +1,6 @@
 "use client";
 import { decrease, increase } from "@/Redux/features/cart/cartSlice";
+import Link from "next/link";
 import { AiOutlineMinusSquare, AiOutlinePlusSquare } from 'react-icons/ai';
 import { useDispatch, useSelector } from "react-redux";
 
@@ -14,7 +15,7 @@ const Cart = () => {
                 &&
                 products.map((product) =>
                     <div className="border border-green-700 rounded-md p-1 mb-3 grid grid-cols-3 items-center">
-                        <h1 className="text-sm"><span className="font-semibold">{product.title}</span></h1>
+                        <h1 className="text-[13px] font-semibold">{product.title}</h1>
                         <div className="flex items-center gap-1 pl-6">
 
                             <button onClick={() => dispatch(decrease(product))}><span><AiOutlineMinusSquare size={20} width={14} /></span></button>
@@ -24,14 +25,23 @@ const Cart = () => {
                             <button onClick={() => dispatch(increase(product))}><span><AiOutlinePlusSquare size={20} width={14} /></span></button>
 
                         </div>
-                        <div className="text-sm text-center"><span className="font-bold">Price: </span>{product.price}</div>
+                        <div className="text-sm text-end"><span className="font-bold">Price: </span>{product.price}</div>
                     </div>
                 )
             }
             {
                 (products.length > 0)
                     ?
-                    <p className="text-end text-sm font-semibold px-1 text-green-600">Total Price: <span className="text-slate-600">{totalPrice}$</span></p>
+
+                    <div className="">
+                        <div className="flex justify-between">
+                            <p className="text-sm font-semibold text-green-600">Total Amount:</p>
+                            <span className="text-slate-600 text-sm font-semibold text-end px-1">${totalPrice}</span>
+                        </div>
+                        <div className="w-full bg-blue-700 mt-4 rounded-md">
+                            <Link href="/checkout" className="w-full block p-2 text-white font-semibold text-center">Checkout</Link>
+                        </div>
+                    </div>
                     :
                     <div className="flex flex-col items-center gap-1">
                         <p className="text-center font-semibold">ADD TO CART YOUR PRODUCT</p>
